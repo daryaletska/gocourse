583f95970449c7790bda8a78e95aac530461361c
Darya Letska <71888665+daryaletska@users.noreply.github.com>
2025-08-17 23:23:57 +0300

    constants

---
+++ basics/constants.go
@@ -0,0 +1,22 @@
+package basics
+
+import "fmt"
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	fmt.Println(PI)
+	fmt.Println(GRAVITY)
+
+	const days int = 7
+	const (
+		MONDAY        = 1
+		TUESDAY       = 2
+		WEDNESDAY     = 3
+		THURSDAY  int = 4
+		// FRIDAY    = 5
+		// SATURDAY  = 6
+		// SUNDAY    = 7
+	)
+}
